281bd1d5ce3b659ff5c5cd150888db8f5d0eab0d
myark <g.ark.zone@gmail.com>
2016-11-20 16:20:17 +0800

    Update index.js
    格式化代码

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+
 import {
     View,
     Text
 } from 'react-native';
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -16,4 +18,5 @@ class App extends Component {
         )
     }
 }
+
 export default App;
