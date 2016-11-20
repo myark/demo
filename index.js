5da173d76bcb20f5331127c493563ede2e64f858
myark <g.ark.zone@gmail.com>
2016-11-20 16:16:13 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <View>
+                <Text>hello,github</Text>
+            </View>
+        )
+    }
+}
+export default App;
